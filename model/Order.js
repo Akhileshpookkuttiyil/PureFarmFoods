0101105eef643227c5226d886a99ac24083602c9
akhilesh <akhileshpookkuttiyil@gmail.com>
2025-01-06 23:46:10 +0530

    fixed order page in userSettings

--- model/Order.js
+++ model/Order.js
@@ -14,6 +14,7 @@ const orderSchema = new mongoose.Schema(
           ref: "Product", // Reference to the Product model
           required: true,
         },
+        size:{type: String,required: true},
         quantity: { type: Number, required: true },
         price: { type: Number, required: true },
       },
